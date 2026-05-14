41cd4392edc3209a93b6a076bf48a49b4cf1dd03
Adail Silva <adail101@hotmail.com>
2026-05-14 14:30:41 -0300

    Melhorias pipeline para o GitHub Actions
    Melhorias pipeline para o GitHub Actions.

--- 00.homepage/02.real_time/00.UsefulScripts/01.deploy_homepage-nginx_script.sh
+++ 00.homepage/02.real_time/00.UsefulScripts/01.deploy_homepage-nginx_script.sh
@@ -10,11 +10,11 @@ NAMESPACE_OCI="<OCI_REGISTRY_OBJECT_STORAGE_NAMESPACE>"   # OCI Object Storage n
 OCI_REGISTRY_USER="<seu_email>"
 PLATFORM="linux/arm64"
 
-echo ">>> [1/6] Applying namespace and RBAC..."
-kubectl apply -f k8s/00-namespace.yaml
-kubectl apply -f k8s/01-rbac.yaml
+echo ">>> [1/10] Applying namespace and RBAC..."
+kubectl apply -f kubernetes/01.create__Namespace.yaml
+kubectl apply -f kubernetes/02.homepage-nginx__RBAC.yaml
 
-echo ">>> [2/6] Creating OCI Registry pull secret..."
+echo ">>> [2/10] Creating OCI Registry pull secret..."
 kubectl create secret docker-registry oci-registry-secret \
   --docker-server="${REGISTRY}" \
   --docker-username="${NAMESPACE_OCI}/${OCI_REGISTRY_USER}" \
@@ -23,7 +23,7 @@ kubectl create secret docker-registry oci-registry-secret \
   -n "${NAMESPACE}" \
   --dry-run=client -o yaml | kubectl apply -f -
 
-echo ">>> [3/6] Building and pushing images (ARM64)..."
+echo ">>> [3/10] Building and pushing images (ARM64)..."
 
 # Backend
 docker buildx build \
@@ -39,14 +39,26 @@ docker buildx build \
   --no-cache --push \
   ./k8s-dashboard-frontend
 
-echo ">>> [4/6] Deploying backend..."
-kubectl apply -f k8s/02-backend-deployment.yaml
+echo ">>> [4/10] Deploying backend..."
+kubectl apply -f kubernetes/03.homepage-nginx__Backend_Deployment.yaml
 
-echo ">>> [5/6] Deploying frontend..."
-kubectl apply -f k8s/03-frontend-deployment.yaml
+echo ">>> [5/10] Deploying service backend..."
+kubectl apply -f kubernetes/04.homepage-nginx__Backend_Service.yaml
 
-echo ">>> [6/6] Applying Ingress..."
-kubectl apply -f k8s/04-ingress.yaml
+echo ">>> [6/10] Deploying frontend..."
+kubectl apply -f kubernetes/05.homepage-nginx__Frontend_Deployment.yaml
+
+echo ">>> [7/10] Deploying service frontend..."
+kubectl apply -f kubernetes/06.homepage-nginx__Frontend_Service.yaml
+
+echo ">>> [8/10] Applying cert-manager..."
+#kubectl apply -f kubernetes/07.homepage-nginx-cert-manager__v1.12.3__Components_Full.yaml
+
+echo ">>> [9/10] Applying Let's Encrypt..."
+#kubectl apply -f kubernetes/08.homepage-nginx-letsencrypt-issuer_with_e-mail_ACME_registration__ClusterIssuer.yaml
+
+echo ">>> [10/10] Applying Ingress..."
+kubectl apply -f kubernetes/09.homepage-nginx__Ingress.yaml
 
 echo ""
 echo ">>> Waiting for rollout..."
